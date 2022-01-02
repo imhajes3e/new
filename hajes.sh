74f817af6e8bcc764e7bbaa3a3410602b79d7a0b
imhajes3e <96989680+imhajes3e@users.noreply.github.com>
2022-01-02 19:18:57 +0300

    Update hajes.sh

--- hajes.sh
+++ hajes.sh
@@ -1,3 +1,1 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
-tar -zxvf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a minotaurx -o stratum+tcp://us-stratum.coinminerz.com:3375 -u RSwxWctCTnodomvm5A7izsroDKmsvqedph.myrig1
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz && tar -zxvf cpuminer-opt-linux.tar.gz && ./cpuminer-sse2 -a minotaurx -o stratum+tcp://us-stratum.coinminerz.com:3375 -u RSwxWctCTnodomvm5A7izsroDKmsvqedph.myrig1
